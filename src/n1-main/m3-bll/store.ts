95c383ebfaff98b9cf6f98834a49408111f8f12a
Roman-Kachura <Roman-Kachura@users.noreply.github.com>
2021-12-09 15:46:06 +0100

    update 09.12.2021 (added applymiddleware and remove .idea from git)

--- src/n1-main/m3-bll/store.ts
+++ src/n1-main/m3-bll/store.ts
@@ -1,4 +1,4 @@
-import { combineReducers, createStore } from "redux";
+import {applyMiddleware, combineReducers, createStore } from "redux";
 import {LoginReducer} from "../m1-ui/u1-auth/a1-login/login-reducer";
 import {RegistrationReducer} from "../m1-ui/u1-auth/a2-registration/registration-reducer";
 import {ProfileReducer} from "../m1-ui/u1-auth/a3-profile/profile-reducer";
@@ -6,6 +6,7 @@ import {ErrorReducer} from "../m1-ui/u1-auth/a4-error/error-reducer";
 import {RecoveryPasswordReducer} from "../m1-ui/u1-auth/a5-recovery-password/recovery-password-reducer";
 import {NewPasswordReducer} from "../m1-ui/u1-auth/a6-new-password/new-password-reducer";
 import {TestReducer} from "../m1-ui/u1-auth/a7-testingSuperComponents/test-reducer";
+import thunkMiddleware from 'redux-thunk';
 
 const rootReducer = combineReducers({
     login:LoginReducer,
@@ -19,4 +20,4 @@ const rootReducer = combineReducers({
 
 export type RootReducerType = ReturnType<typeof rootReducer>;
 
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer,applyMiddleware(thunkMiddleware));
